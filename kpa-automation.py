f74a1c591944f8c306d4caa99b8622e7d09c4166
gregorywpower <gregorywpower@gmail.com>
2021-05-07 16:27:37 -0400

    able to pull data from report into dataframe

--- kpa-automation.py
+++ kpa-automation.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
@@ -60,8 +61,6 @@ def login_into_dash(json_target_file):
     browser.find_element_by_name("ctl00$ContentPlaceHolder1$Password").send_keys(password)
     browser.find_element_by_name("ctl00$ContentPlaceHolder1$btnLogin").click()
 
-login_into_dash("DASHLoginInfo.json") # Here we are calling the function we defined in this block.
-
 def navigate_to_BES_Dashboard_Jobs():
     browser.get("http://sem.myirate.com/Reports/AdHoc_View.aspx?id=1314")
     yesterday_start_point = datetime.strftime(datetime.now() - timedelta(1), '%m/%d/%y 12:00 AM')
@@ -71,10 +70,48 @@ def navigate_to_BES_Dashboard_Jobs():
     browser.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[6]/div[4]/ul/li/ul/li/div/div[2]/div[1]/table/tbody/tr/td[1]/span/input[1]").send_keys(yesterday_start_point)
     browser.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[6]/div[4]/ul/li/ul/li/div/div[2]/div[2]/table/tbody/tr/td[1]/span/input[1]").send_keys(yesterday_end_point)
 
-navigate_to_BES_Dashboard_Jobs()
+def read_table():
+    dataframe = pd.DataFrame()
+        # Here we define the number of pages we have to scrape through.
+        
+    items_and_pages_element = browser.find_element_by_class_name("rgInfoPart").text
+    digits_list = []
+    pattern = r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
+    if re.search(pattern, items_and_pages_element) is not None:
+       for catch in re.finditer(pattern, items_and_pages_element):
+           # print(catch[0])
+           digits_list.append(catch[0])
+    else:
+       print("Something is broken.")
+
+   # print(digits_list)
+
+    items = int(digits_list[0])
+    pages = int(digits_list[1])
+    print("Number of items: " + str(items))
+    print("Number of pages: " + str(pages))
+ 
+    while int(len(dataframe.index)) < items:
+        try:
+            WebDriverWait(browser,5).until(EC.visibility_of_element_located((By.ID,'ctl00_ContentPlaceHolder1_rgReport_ctl00__0')))
+        finally:
+            table_list = browser.find_elements_by_class_name('rgClipCells')
+            table_we_want = table_list[1].get_attribute('outerHTML')
+            dataframe = dataframe.append(pd.read_html(table_we_want),ignore_index=True)
+            print(len(dataframe))
+        try:
+            WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CLASS_NAME,"rgPageNext"))).click()
+        except:
+            WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.CLASS_NAME,"rgPageNext"))).click()
+    else:
+        print("We are done scraping.")
+        dataframe.rename(columns={0:'DASH ID', 1:'Builder', 2:'ServiceType', 3:'ServiceDate',4:'Team Member',5:'Price'}, inplace=True)
+        print(dataframe)
+        print(len(dataframe.index))
+
+
 
 def navigate_to_files_page(DASH_ID):
-
     print(f"Current Rating ID Being QA'ed is: " + str(DASH_ID))
     browser.get(f"http://sem.myirate.com/Jobs/NewConst_Edit_File.aspx?id=1&j=" + str(DASH_ID)) # This line navigates to the files page in DASH.
 
@@ -102,3 +139,9 @@ def navigate_to_files_page(DASH_ID):
             print(False)
             print("Uploading Certificate")
     print(file_label_list)
+
+def main():
+    login_into_dash("DASHLoginInfo.json") # Here we are calling the function we defined in this block.
+    navigate_to_BES_Dashboard_Jobs()
+    read_table()
+main()
