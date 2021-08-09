e714517d0a4b6a259a327743ba67f8e7bc204607
gregorywpower <gregorywpower@gmail.com>
2021-08-09 16:55:34 -0400

    added descriptions to bundler

--- DASHNextGen_KPI_Bundler.py
+++ DASHNextGen_KPI_Bundler.py
@@ -1,4 +1,4 @@
-import DASHNextGen_Service_DateClicker
-import DASHNextGen_Service_Report_date
-import DASHNextGen_job_read_Service_Report_Export
-import DASHNextGen_all_files
+import DASHNextGen_Service_DateClicker #Ensures the date range for the service report is updated.
+import DASHNextGen_Service_Report_date #Harvests the service data we require.
+import DASHNextGen_job_read_Service_Report_Export #Reads the job data of the harvested services.
+import DASHNextGen_all_files #Reads the files that have been uploaded to each DASH ID, excluding "Other Files"
