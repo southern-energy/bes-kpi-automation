0d9d00a03c051c2f81eba8868c800b3f1e0e0d0b
gregorywpower <gregorywpower@gmail.com>
2021-08-12 16:25:35 -0400

    adding logging to DASHNextGen_KPI_Bundler

--- DASHNextGen_KPI_Bundler.py
+++ DASHNextGen_KPI_Bundler.py
@@ -1,4 +1,13 @@
-import DASHNextGen_Service_DateClicker #Ensures the date range for the service report is updated.
-import DASHNextGen_Service_Report_date #Harvests the service data we require.
-import DASHNextGen_job_read_Service_Report_Export #Reads the job data of the harvested services.
-import DASHNextGen_all_files #Reads the files that have been uploaded to each DASH ID, excluding "Other Files"
+import datetime
+
+try:
+    print("""Running All of the Scripts in the Bundler\n""")
+    import DASHNextGen_Service_DateClicker #Ensures the date range for the service report is updated.
+    import DASHNextGen_Service_Report_date #Harvests the service data we require.
+    import DASHNextGen_job_read_Service_Report_Export #Reads the job data of the harvested services.
+    import DASHNextGen_all_files #Reads the files that have been uploaded to each DASH ID, excluding "Other Files"
+      
+except Exception as e:
+        with open("KPI_Bundler_Log.csv", "a") as log:
+                 log.write('KPI_Bundler,'+str(e) +','+ str(datetime.datetime.now()) +'\n')
+        print('Error: ' + str(e))
