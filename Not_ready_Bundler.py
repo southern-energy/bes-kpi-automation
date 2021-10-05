38fe78321bdab96ca68f0ea26af5336e12efff2d
gregorywpower <gregorywpower@gmail.com>
2021-10-05 16:24:20 -0400

    added logging to Not_ready_Bundler.py

--- Not_ready_Bundler.py
+++ Not_ready_Bundler.py
@@ -1,3 +1,12 @@
-import Not_Ready_cleanup_job
-import Not_Ready_cleanup_all_files
-import Low_KPI_Rerun_File_Update
+import datetime
+
+try: 
+    print("""Running All of the Scripts in the Not Ready Bundler\n""")
+    import Not_Ready_cleanup_job
+    import Not_Ready_cleanup_all_files
+    import Low_KPI_Rerun_File_Update
+
+except Exception as e:
+        with open("Not_ready_Bundler_Log.csv", "a") as log:
+                 log.write('Not_ready_Bundler_Log.csv,'+ str(e) + ',' + str(datetime.datetime.now()) +'\n')
+        print('Error: ' + str(e))
