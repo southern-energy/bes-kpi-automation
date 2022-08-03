c3a1fa9408ab53fc6ca48aade75f4fea31e107d5
Gregory <gregorywpower@gmail.com>
2022-08-03 06:57:13 -0400

    make logging convert to string

--- Not_ready_Bundler.py
+++ Not_ready_Bundler.py
@@ -17,5 +17,5 @@ try:
 except Exception as e:
         with open("LOG_Not_ready_Bundler.csv", "a") as log:
                  log.write('LOG_Not_ready_Bundler.csv,'+ str(e) + ',' + str(datetime.datetime.now()) +'\n')
-                 log.write(logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(funcName)s %(levelname)s - %(message)s','%m-%d %H:%M:%S')+'\n')
+                 log.write(str(logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(funcName)s %(levelname)s - %(message)s','%m-%d %H:%M:%S'))+'\n')
         print('Error: ' + str(e))
