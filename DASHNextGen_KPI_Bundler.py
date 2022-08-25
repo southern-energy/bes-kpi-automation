912251a6a0b35e73d9a06d9edcf32f037f82850f
Gregory <gregorywpower@gmail.com>
2022-08-25 14:16:44 -0400

    measuring time

--- DASHNextGen_KPI_Bundler.py
+++ DASHNextGen_KPI_Bundler.py
@@ -1,8 +1,11 @@
 import datetime
 import urllib.request
 import logging
+import time
 
 urlString = 'http://pshmn.com/p2JnYuS' # Replace string with 'http://pshmn.com/p2JnYuS' if you want Pushmon notifications.
+
+start_time = time.perf_counter()
 
 try:
     print("""Running All of the Scripts in the Bundler\n""")
@@ -19,3 +22,13 @@ except Exception as e:
                  log.write('LOG_KPI_Bundler,'+str(e) +','+ str(datetime.datetime.now()) +'\n')
                  log.write(str(logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(funcName)s %(levelname)s - %(message)s','%m-%d %H:%M:%S'))+'\n')
         print('Error: ' + str(e))
+
+end_time = time.perf_counter()
+
+time_spent_running_manually = end_time - start_time
+
+time_spent_running_manually_in_minutes = (time_spent_running_manually / 60)
+
+print("You've spent " + str(time_spent_running_manually_in_minutes) + " minutes running this manually.")
+
+print("Finished Running at", time.ctime(time.time()))
